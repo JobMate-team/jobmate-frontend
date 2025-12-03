14e30705abcf57cdcfea023a12b4bf6d5fc1f7c6
정찬원 <jcw0522@gachon.ac.kr>
2025-12-03 21:25:44 +0900

    [#7] feat: 질문 선택 드롭다운 구현

--- src/pages/CoachingPage.tsx
+++ src/pages/CoachingPage.tsx
@@ -1,25 +1,38 @@
-import Button from '@/components/common/Button';
-import { ChevronDown } from 'lucide-react';
-import { FaAngleRight } from 'react-icons/fa6';
+import { useState } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
+import Button from '@/components/common/Button';
+import DropDown from '@/components/ui/Dropdown';
+import { FaAngleRight } from 'react-icons/fa6';
 
 const CoachingPage = () => {
+  const [selectedJob, setSelectedJob] = useState<string | null>(null);
+  const [selectedQuestion, setSelectedQuestion] = useState<string | null>(null);
+
+  const jobItems = ['기획', '개발', '마케팅', '디자인', '영업', '인사'];
+  const basicItems = ['하이'];
+
   return (
-    <div className='space-y-5'>
-      <div className='bg-white rounded-xl px-6 py-4 cursor-pointer border border-[#E5E5E5] flex flex-col gap-4'>
+    <div className='space-y-5 relative'>
+      <div className='bg-white rounded-xl px-6 py-4 border border-[#E5E5E5] flex flex-col gap-4'>
         <p className='font-semibold'>직무 선택</p>
-        <div className='bg-[#F3F3F5] rounded-lg p-2.5 px-4 text-sm flex justify-between'>
-          <p className='text-[#717182]'>직무 선택</p>
-          <ChevronDown size={20} stroke='#717182' />
-        </div>
+        <DropDown
+          items={jobItems}
+          selected={selectedJob}
+          placeholder='직무 선택'
+          onSelect={(job) => setSelectedJob(job)}
+        />
       </div>
-      <div className='bg-white rounded-xl px-6 py-4 cursor-pointer border border-[#E5E5E5] flex flex-col gap-4'>
+
+      <div className='bg-white rounded-xl px-6 py-4 border border-[#E5E5E5] flex flex-col gap-4'>
         <p className='font-semibold'>면접 질문</p>
-        <div className='bg-[#F3F3F5] rounded-lg p-2.5 px-4 text-sm mb-5 flex justify-between'>
-          <p className='text-[#717182]'>기본 질문 선택</p>
-          <ChevronDown size={20} stroke='#717182' />
-        </div>
-        <p className='font-semibold'>또는 직접 입력</p>
+        <DropDown
+          items={basicItems}
+          selected={selectedQuestion}
+          placeholder='기본 질문 선택'
+          onSelect={(question) => setSelectedQuestion(question)}
+        />
+
+        <p className='font-semibold mt-4'>또는 직접 입력</p>
         <TextareaAutosize
           minRows={3}
           placeholder='면접 질문을 직접 입력하세요'
