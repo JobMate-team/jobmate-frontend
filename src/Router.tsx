87b0c0b77dd390b93411d3b37dd152d032fd2c41
정찬원 <jcw0522@gachon.ac.kr>
2025-12-05 14:00:15 +0900

    fix : 오류 파일 삭제

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
-import Homepage from './pages/HomePage';
+import Homepage from './pages/Homepage';
 import OnboardingPage from './pages/OnboardingPage';
 import LoginPage from './pages/LoginPage';
 import SelectRolePage from './pages/SelectRolePage';
