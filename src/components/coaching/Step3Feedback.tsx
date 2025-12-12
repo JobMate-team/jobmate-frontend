e7886f8078cf00c490e6377ac446dafb90610c8c
정찬원 <jcw0522@gachon.ac.kr>
2025-12-13 03:43:48 +0900

    feat: 피드백 단계 컴포넌트 분리

--- src/components/coaching/Step3Feedback.tsx
+++ src/components/coaching/Step3Feedback.tsx
@@ -2,73 +2,84 @@ import Button from '@/components/common/Button';
 import { LuLightbulb, LuRotateCcw } from 'react-icons/lu';
 import { FiSave } from 'react-icons/fi';
 import { showToast } from '@/utils/toast';
+import type React from 'react';
 
 interface Props {
-  question: string;
+  customQuestion: string;
+  selectedQuestion: string | null;
   customAnswer: string;
   feedback: string;
-  showExample: boolean;
-  // eslint-disable-next-line no-unused-vars
-  setShowExample: (v: boolean) => void;
-  resetFlow: () => void;
+  showExampleAnswer: boolean;
+  setShowExampleAnswer: React.Dispatch<React.SetStateAction<boolean>>;
+  resetCoaching: () => void;
 }
 
 const Step3Feedback = ({
-  question,
+  customQuestion,
+  selectedQuestion,
   customAnswer,
   feedback,
-  showExample,
-  setShowExample,
-  resetFlow,
+  showExampleAnswer,
+  setShowExampleAnswer,
+  resetCoaching,
 }: Props) => {
   return (
     <>
-      <div className='bg-black text-white rounded-xl p-6'>
+      <div className='bg-black rounded-xl p-6 flex flex-col gap-4 text-white'>
         <p className='font-semibold'>선택된 질문</p>
-        <p className='leading-5 whitespace-pre-wrap'>{question}</p>
+        <p className='leading-5 whitespace-pre-wrap'>{customQuestion || selectedQuestion}</p>
       </div>
 
-      <div className='bg-white rounded-xl p-6 border flex flex-col gap-4'>
+      <div className='bg-white rounded-xl p-6 flex flex-col gap-4 border border-[#E5E5E5]'>
         <p className='font-semibold'>내 답변</p>
         <p className='leading-5 whitespace-pre-wrap'>{customAnswer}</p>
       </div>
 
-      <div className='bg-white rounded-xl p-6 border flex flex-col gap-4'>
-        <p className='font-semibold'>AI 피드백</p>
-        <div className='bg-[#F3F3F5] rounded-lg p-4 leading-5 whitespace-pre-wrap'>{feedback}</div>
+      <div className='bg-white rounded-xl px-6 py-4 border border-[#E5E5E5] flex flex-col gap-4'>
+        <p className='font-semibold py-2'>AI 피드백</p>
+        <div className='bg-[#F3F3F5] rounded-lg p-4 max-sm:text-sm mb-2 leading-5 whitespace-pre-wrap'>
+          넌 안돼 망할거라 우우~
+        </div>
       </div>
 
-      <div className='flex max-sm:flex-col gap-5 mt-10'>
+      <div className='flex max-sm:flex-col items-center gap-5 sm:max-w-[80%] sm:mx-auto mt-10'>
         <Button
           type='button'
-          className='w-full bg-black text-white'
-          onClick={() => setShowExample(!showExample)}
+          className='w-full bg-black text-white px-4 py-3 gap-2'
+          onClick={() => setShowExampleAnswer((prev) => !prev)}
         >
-          <LuLightbulb />
-          {showExample ? '모범 답변 숨기기' : '모범 답변 보기'}
+          <LuLightbulb size={18} />
+          {showExampleAnswer ? '모범 답변 숨기기' : '모범 답변 예시 보기'}
         </Button>
 
-        <Button type='button' className='w-full bg-black text-white' onClick={resetFlow}>
-          <LuRotateCcw /> 새로운 질문 연습하기
+        <Button
+          type='button'
+          className='w-full bg-black text-white px-4 py-3 gap-2'
+          onClick={resetCoaching}
+        >
+          <LuRotateCcw size={18} /> 새로운 질문 연습하기
         </Button>
       </div>
 
-      {showExample && (
-        <div className='bg-white rounded-xl p-6 border flex flex-col gap-4'>
-          <div className='flex justify-between items-center'>
+      {showExampleAnswer && (
+        <div className='bg-white rounded-xl px-6 py-4  border border-[#E5E5E5] flex flex-col gap-4'>
+          <div className='flex items-center justify-between'>
             <p className='font-semibold'>📝 모범 답변 예시</p>
             <Button
               type='button'
-              className='border border-black/10 bg-white text-sm p-2 px-3'
+              className='border border-black/10 text-sm font-medium gap-1.5 p-2 px-3 bg-white'
               onClick={() => {
                 navigator.clipboard.writeText(feedback);
                 showToast.success('히스토리에 저장되었습니다');
               }}
             >
-              <FiSave /> 저장
+              <FiSave size={18} />
+              저장
             </Button>
           </div>
-          <div className='bg-[#F3F3F5] rounded-lg p-4 whitespace-pre-wrap'>{feedback}</div>
+          <div className='bg-[#F3F3F5] rounded-lg p-4 max-sm:text-sm mb-2 leading-5 whitespace-pre-wrap'>
+            {feedback}
+          </div>
         </div>
       )}
     </>
--- src/pages/CoachingPage.tsx
+++ src/pages/CoachingPage.tsx
@@ -1,14 +1,12 @@
 import { useState } from 'react';
-import Button from '@/components/common/Button';
 import { showToast } from '@/utils/toast';
-import { FiSave } from 'react-icons/fi';
-import { LuLightbulb, LuRotateCcw } from 'react-icons/lu';
 import { useAtom } from 'jotai';
 import { pageAtom } from '@/atoms';
 import { Outlet } from 'react-router-dom';
 import Step1Select from '@/components/coaching/Step1Select';
 import Step2Answer from '@/components/coaching/Step2Answer';
 import CoachStepHeader from '@/components/coaching/CoachStepHeader';
+import Step3Feedback from '@/components/coaching/Step3Feedback';
 
 const CoachingPage = () => {
   const [page, setPage] = useAtom(pageAtom);
@@ -85,65 +83,15 @@ const CoachingPage = () => {
       )}
 
       {page === 3 && (
-        <>
-          <div className='bg-black rounded-xl p-6 flex flex-col gap-4 text-white'>
-            <p className='font-semibold'>선택된 질문</p>
-            <p className='leading-5 whitespace-pre-wrap'>{customQuestion || selectedQuestion}</p>
-          </div>
-
-          <div className='bg-white rounded-xl p-6 flex flex-col gap-4 border border-[#E5E5E5]'>
-            <p className='font-semibold'>내 답변</p>
-            <p className='leading-5 whitespace-pre-wrap'>{customAnswer}</p>
-          </div>
-
-          <div className='bg-white rounded-xl px-6 py-4 border border-[#E5E5E5] flex flex-col gap-4'>
-            <p className='font-semibold py-2'>AI 피드백</p>
-            <div className='bg-[#F3F3F5] rounded-lg p-4 max-sm:text-sm mb-2 leading-5 whitespace-pre-wrap'>
-              넌 안돼 망할거라 우우~
-            </div>
-          </div>
-
-          <div className='flex max-sm:flex-col items-center gap-5 sm:max-w-[80%] sm:mx-auto mt-10'>
-            <Button
-              type='button'
-              className='w-full bg-black text-white px-4 py-3 gap-2'
-              onClick={() => setShowExampleAnswer((prev) => !prev)}
-            >
-              <LuLightbulb size={18} />
-              {showExampleAnswer ? '모범 답변 숨기기' : '모범 답변 예시 보기'}
-            </Button>
-
-            <Button
-              type='button'
-              className='w-full bg-black text-white px-4 py-3 gap-2'
-              onClick={resetCoaching}
-            >
-              <LuRotateCcw size={18} /> 새로운 질문 연습하기
-            </Button>
-          </div>
-
-          {showExampleAnswer && (
-            <div className='bg-white rounded-xl px-6 py-4  border border-[#E5E5E5] flex flex-col gap-4'>
-              <div className='flex items-center justify-between'>
-                <p className='font-semibold'>📝 모범 답변 예시</p>
-                <Button
-                  type='button'
-                  className='border border-black/10 text-sm font-medium gap-1.5 p-2 px-3 bg-white'
-                  onClick={() => {
-                    navigator.clipboard.writeText(feedback);
-                    showToast.success('히스토리에 저장되었습니다');
-                  }}
-                >
-                  <FiSave size={18} />
-                  저장
-                </Button>
-              </div>
-              <div className='bg-[#F3F3F5] rounded-lg p-4 max-sm:text-sm mb-2 leading-5 whitespace-pre-wrap'>
-                {feedback}
-              </div>
-            </div>
-          )}
-        </>
+        <Step3Feedback
+          customQuestion={customQuestion}
+          selectedQuestion={selectedQuestion}
+          customAnswer={customAnswer}
+          feedback={feedback}
+          showExampleAnswer={showExampleAnswer}
+          setShowExampleAnswer={setShowExampleAnswer}
+          resetCoaching={resetCoaching}
+        />
       )}
 
       <Outlet />
